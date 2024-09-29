bf155d3df60da3ffebc6736ee7d9628d41d3fb6c
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-09-28 20:14:26 -0400

    Fix album fetch

--- app/components/AlbumVinyl.tsx
+++ app/components/AlbumVinyl.tsx
@@ -1,11 +1,32 @@
 'use client'
 import { useAudioContext } from '@/app/context/appState'
 import { cn } from '@/app/lib/utils'
-import { Songs } from '../lib/api-response'
+import { Song } from '../lib/api-response'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+
 
 export default function AlbumVinyl() {
+  const [Songs, setSongs] = useState<Song[]>([])
   const { pause, activeIndex } = useAudioContext()
+
+  useEffect(() => {
+    const fetchSongsList = async () => {
+      try {
+        const response = await fetch('https://discofy-app-data01.s3.amazonaws.com/songs/songs.json');
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data: any = await response.json();
+        setSongs(data);
+      } catch (err: any) {
+        console.log(err)
+      }
+    };
+    fetchSongsList();
+  }, []);
 
   return (
     <div className="flex flex-col gap-4">
@@ -17,9 +38,9 @@ export default function AlbumVinyl() {
           height={315}
           src={
             activeIndex !== undefined &&
-            activeIndex !== null &&
-            activeIndex >= 0 &&
-            activeIndex < Songs.length
+              activeIndex !== null &&
+              activeIndex >= 0 &&
+              activeIndex < Songs.length
               ? Songs[activeIndex]?.image
               : ''
           }
@@ -28,14 +49,13 @@ export default function AlbumVinyl() {
         <div
           style={{
             backgroundImage: `url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/83141/vinyl.png'),
-                       url(${
-                         activeIndex !== undefined &&
-                         activeIndex !== null &&
-                         activeIndex >= 0 &&
-                         activeIndex < Songs.length
-                           ? Songs[activeIndex]?.image
-                           : ''
-                       })`
+                       url(${activeIndex !== undefined &&
+                activeIndex !== null &&
+                activeIndex >= 0 &&
+                activeIndex < Songs.length
+                ? Songs[activeIndex]?.image
+                : ''
+              })`
           }}
           className={cn(
             'vinyl rounded-full w-[300px] h-[300px] absolute top-[5px] left-2',
