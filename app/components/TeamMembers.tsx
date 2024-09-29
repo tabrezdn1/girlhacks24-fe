a897ac5c73ae99f8548d239483d044e3c26f1335
richas-bsingh <72738297+richas0719@users.noreply.github.com>
2024-09-29 11:57:56 -0400

    adding linkedin and github links

--- app/components/TeamMembers.tsx
+++ app/components/TeamMembers.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { Linkedin , Github} from 'lucide-react';
 
 const teamMembers = [
   { 
@@ -9,28 +10,36 @@ const teamMembers = [
     name: 'Richa Singh', 
     role: 'MS in CS @ NJIT', 
     image: '/richa.jpeg',
-    skills: ['Python/R', 'Data Visualization and Analytics', 'Cloud']
+    skills: ['Python/R', 'Data Visualization and Analytics', 'Cloud'],
+    linkedin: 'https://www.linkedin.com/in/richa-singh-78935438/',
+    github: 'https://github.com/richa-bsingh'
   },
   { 
     id: 2, 
     name: 'Tabrez Shaik', 
     role: 'MS in CS @ NJIT', 
     image: '/tab_shaikh.jpeg',
-    skills: ['AI/ML', 'Full Stack Engineer', 'Cloud Integration']
+    skills: ['AI/ML', 'Full Stack Engineer', 'Cloud Integration'],
+    linkedin: 'https://www.linkedin.com/in/shaik-tabrez/',
+    github: 'https://github.com/tabrezdn1'
   },
   { 
     id: 3, 
     name: 'Jonathan Grossman', 
     role: 'MS in DS @ NJIT', 
     image: '/jon.jpeg',
-    skills: ['Full Stack Engineer', 'Python', 'Prompt Engineering/LLM']
+    skills: ['Full Stack Engineer', 'Python', 'Prompt Engineering/LLM'],
+    linkedin: 'https://www.linkedin.com/in/jonathan-grossman/',
+    github: 'https://github.com/jgwentworth92'
   },
   { 
     id: 4, 
     name: 'Vishal Khairnar', 
     role: 'MS in CS @ NJIT', 
     image: '/vishal.jpeg',
-    skills: ['Cloud', 'Big Data', 'Data Engineer']
+    skills: ['Cloud', 'Big Data', 'Data Engineer'],
+    linkedin: 'https://www.linkedin.com/in/vishal-chhagan-khairnar-39383b100/',
+    github: 'https://github.com/vishal2609'
   },
 ];
 
@@ -50,13 +59,31 @@ const TeamMembers = () => {
           </div>
           <h3 className="text-xl font-semibold mb-2">{member.name}</h3>
           <p className="text-gray-600 mb-4">{member.role}</p>
-          <div className="flex flex-wrap justify-center gap-2">
+          <div className="flex flex-wrap justify-center gap-2 mb-4">
             {member.skills.map((skill, index) => (
               <span key={index} className="px-2 py-1 bg-blue-100 text-blue-800 text-xs font-medium rounded-full">
                 {skill}
               </span>
             ))}
           </div>
+          <a 
+            href={member.linkedin} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="flex items-center text-blue-600 hover:text-blue-800"
+          >
+            <Linkedin size={20} className="mr-2" />
+            LinkedIn Profile
+          </a>
+          <a 
+              href={member.github} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="flex items-center text-gray-700 hover:text-gray-900"
+            >
+              <Github size={20} className="mr-2" />
+              GitHub
+            </a>
         </div>
       ))}
     </div>
